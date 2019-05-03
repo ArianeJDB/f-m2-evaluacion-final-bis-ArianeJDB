028545bc3606eece450208936951372d6020f83d
arijdb <ariane.jurado@gmail.com>
2019-05-03 12:13:45 +0200

    add adalab side as default

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,36 +1,15 @@
 'use strict';
 
 const input = document.querySelectorAll('input');
-
 const btn = document.querySelector('.btn');
 const list = document.querySelector('.list');
 
 for (const items of input) {
-  //const startGame = () => {
-  const elementDown = document.createElement('li');
-  const imgDown = document.createElement('img');
-  imgDown.setAttribute('class', 'face-down');
-  imgDown.setAttribute(
-    'src',
-    'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB'
-  );
-  elementDown.appendChild(imgDown);
-  list.appendChild(elementDown);
-  //};
   const chooseCards = e => {
     const trigger = e.currentTarget;
 
     const value = trigger.value;
     const startGame = () => {
-      const elementDown = document.createElement('li');
-      const imgDown = document.createElement('img');
-      imgDown.setAttribute('class', 'face-down');
-      imgDown.setAttribute(
-        'src',
-        'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB'
-      );
-      elementDown.appendChild(imgDown);
-      list.appendChild(elementDown);
       const api = `https://raw.githubusercontent.com/Adalab/cards-data/master/${value}.json`;
       fetch(api)
         .then(response => response.json())
@@ -42,14 +21,36 @@ for (const items of input) {
 
             const imgUp = document.createElement('img');
             imgUp.setAttribute('src', image);
-            imgUp.setAttribute('class', 'face-up');
+            imgUp.setAttribute('class', 'hidden');
+
+            // imgUp.setAttribute('class', 'face-down');
             const elementUp = document.createElement('li');
+            elementUp.setAttribute('class', 'face-down');
             elementUp.appendChild(imgUp);
             list.appendChild(elementUp);
+
+            // if (imgUp.classList.contains('face-down')) {
+            //   imgUp.classList.remove('hidden', 'face-down');
+            //   imgUp.classList.add('face-up');
+            // } else {
+            //   imgUp.classList.add('hidden', 'face-down');
+            //   imgUp.classList.remove('face-up');
+            // }
           }
+
         });
     };
     btn.addEventListener('click', startGame);
   };
   items.addEventListener('click', chooseCards);
 }
+
+// const elementDown = document.createElement('li');
+//       const imgDown = document.createElement('img');
+//       imgDown.setAttribute('class', 'face-down');
+//       imgDown.setAttribute(
+//         'src',
+//         'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB'
+//       );
+//       elementDown.appendChild(imgDown);
+//       list.appendChild(elementDown);
