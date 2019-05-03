7ffdb2c822cbf023ed2daa54a4dd56febedfa9a8
arijdb <ariane.jurado@gmail.com>
2019-05-03 10:05:40 +0200

    fix problem with numbers acc

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,46 +3,53 @@
 const input = document.querySelectorAll('input');
 
 const btn = document.querySelector('.btn');
-const faceUp = document.querySelector('.face__side');
-const faceDown = document.querySelector('.back__side');
-
-fetch('https://raw.githubusercontent.com/Adalab/cards-data/master/4.json')
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < data.length; i++) {
-      const item = data[i];
-
-      const { image } = item;
-
-      const img = document.createElement('img');
-      img.setAttribute('src', image);
-      console.log(img);
-
-      const li = document.createElement('li');
-      li.appendChild(img);
-      faceUp.appendChild(li);
-    }
-  });
+const list = document.querySelector('.list');
 
 for (const items of input) {
+  //const startGame = () => {
+  const elementDown = document.createElement('li');
+  const imgDown = document.createElement('img');
+  imgDown.setAttribute('class', 'face-down');
+  imgDown.setAttribute(
+    'src',
+    'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB'
+  );
+  elementDown.appendChild(imgDown);
+  list.appendChild(elementDown);
+  //};
   const chooseCards = e => {
     const trigger = e.currentTarget;
 
     const value = trigger.value;
+    const startGame = () => {
+      const elementDown = document.createElement('li');
+      const imgDown = document.createElement('img');
+      imgDown.setAttribute('class', 'face-down');
+      imgDown.setAttribute(
+        'src',
+        'https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB'
+      );
+      elementDown.appendChild(imgDown);
+      list.appendChild(elementDown);
+      const api = `https://raw.githubusercontent.com/Adalab/cards-data/master/${value}.json`;
+      fetch(api)
+        .then(response => response.json())
+        .then(data => {
+          list.innerHTML = '';
+          for (let i = 0; i < data.length; i++) {
+            const item = data[i];
+            const { image } = item;
 
+            const imgUp = document.createElement('img');
+            imgUp.setAttribute('src', image);
+            imgUp.setAttribute('class', 'face-up');
+            const elementUp = document.createElement('li');
+            elementUp.appendChild(imgUp);
+            list.appendChild(elementUp);
+          }
+        });
+    };
+    btn.addEventListener('click', startGame);
   };
   items.addEventListener('click', chooseCards);
 }
-
-const api = `https://raw.githubusercontent.com/Adalab/cards-data/master/${value}.json`;
-fetch(api)
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < data.length; i++) {
-      const item = data[i];
-      const { image } = item;
-      const img = document.createElement('img');
-      img.setAttribute('src', image);
-      faceUp.appendChild.img;
-    }
-  });
